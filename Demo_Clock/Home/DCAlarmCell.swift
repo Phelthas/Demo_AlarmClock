17b8144fcfe520ee20bfd3a918e689fb663e7d93
dlwxxxdlw <673298622@qq.com>
2017-02-13 17:23:30 +0800

    Swift3.0版本的闹钟demo

--- Demo_Clock/Home/DCAlarmCell.swift
+++ Demo_Clock/Home/DCAlarmCell.swift
@@ -22,35 +22,35 @@ class DCAlarmCell: UITableViewCell {
     
     @IBOutlet weak var alarmSwith: UISwitch!
     
-    private var alarm: DCAlarm?
+    fileprivate var alarm: DCAlarm?
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
-    override func setSelected(selected: Bool, animated: Bool) {
+    override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
     }
 
     
-    func configWithAlarm(alarm: DCAlarm, indexPath: NSIndexPath) {
+    func configWithAlarm(_ alarm: DCAlarm, indexPath: IndexPath) {
         self.alarm = alarm
-        let dateFormatter = NSDateFormatter()
+        let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "HH:mm"
         if let date = alarm.alarmDate {
-            self.dateLabel.text = dateFormatter.stringFromDate(date)
+            self.dateLabel.text = dateFormatter.string(from: date as Date)
         }
         
         self.descriptionLabel.text = alarm.descriptionText
-        self.alarmSwith.on = alarm.alarmOn
+        self.alarmSwith.isOn = alarm.alarmOn
     }
     
-    @IBAction func handleSwitchTapped(sender: UISwitch) {
+    @IBAction func handleSwitchTapped(_ sender: UISwitch) {
         if let tempAlarm = self.alarm {
-            if sender.on {
+            if sender.isOn {
                 tempAlarm.turnOnAlarm()
             } else {
                 tempAlarm.turnOffAlarm()
--- Demo_Clock/LXMKit/LXMBaseViewController.swift
+++ Demo_Clock/LXMKit/LXMBaseViewController.swift
@@ -19,12 +19,12 @@ class LXMBaseViewController: UIViewController {
         #if DEBUG
             
             let leftSwipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(LXMBaseViewController.handleLeftSwipeGesture))
-            leftSwipeGesture.direction = .Left
+            leftSwipeGesture.direction = .left
             leftSwipeGesture.numberOfTouchesRequired = kDefaultNumberOfTouchesRequired
             self.view.addGestureRecognizer(leftSwipeGesture)
             
             let rightSwipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(LXMBaseViewController.handleRightSwipeGesture))
-            rightSwipeGesture.direction = .Right
+            rightSwipeGesture.direction = .right
             rightSwipeGesture.numberOfTouchesRequired = kDefaultNumberOfTouchesRequired
             self.view.addGestureRecognizer(rightSwipeGesture)
             
@@ -44,17 +44,17 @@ class LXMBaseViewController: UIViewController {
 // MARK: - Action
 extension LXMBaseViewController {
     
-    func handleLeftSwipeGesture(sender: UISwipeGestureRecognizer) -> Void {
+    func handleLeftSwipeGesture(_ sender: UISwipeGestureRecognizer) -> Void {
         DLog("handleLeftSwipeGesture")
         let consoleViewController = LXMConsoleViewController()
         consoleViewController.hidesBottomBarWhenPushed = true
-        self.presentViewController(consoleViewController, animated: true, completion: { () -> Void in
+        self.present(consoleViewController, animated: true, completion: { () -> Void in
             
         })
         
     }
     
-    func handleRightSwipeGesture(sender: UISwipeGestureRecognizer) -> Void {
+    func handleRightSwipeGesture(_ sender: UISwipeGestureRecognizer) -> Void {
         DLog("handleRightSwipeGesture")
     }
 
@@ -65,28 +65,28 @@ extension UIViewController {
     /**
      如果storyboardId跟类名相同，可以直接用这个方法
      */
-    class func swift_loadFromStoryboard(storyboardName: String) -> Self {
+    class func swift_loadFromStoryboard(_ storyboardName: String) -> Self {
         return instantiateFromStoryboardHelper(storyboardName, storyboardId: nil)
     }
 
     /**
      类名跟storyboardId不一样时，需要传入storyboardName和storyboardId
      */
-    class func swift_loadFromStoryboard(storyboardName: String, storyboardId: String) -> Self {
+    class func swift_loadFromStoryboard(_ storyboardName: String, storyboardId: String) -> Self {
         return instantiateFromStoryboardHelper(storyboardName, storyboardId: storyboardId)
     }
     
-    private class func instantiateFromStoryboardHelper<T>(storyboardName: String, storyboardId: String?) -> T {
+    fileprivate class func instantiateFromStoryboardHelper<T>(_ storyboardName: String, storyboardId: String?) -> T {
         let storyboard = UIStoryboard(name: storyboardName, bundle: nil)
         var identifier : String
         if let temp = storyboardId {
             identifier = temp
         } else {
             let tempString = NSStringFromClass(self.classForCoder())
-            let tempArray = tempString.componentsSeparatedByString(".")
+            let tempArray = tempString.components(separatedBy: ".")
             identifier = tempArray.last!
         }
-        let controller = storyboard.instantiateViewControllerWithIdentifier(identifier) as! T
+        let controller = storyboard.instantiateViewController(withIdentifier: identifier) as! T
         return controller
     }
 }
